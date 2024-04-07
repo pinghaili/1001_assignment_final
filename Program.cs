8544e528b62f03446fb637dad81dffc1778cf5c3
PinghaiLi <pinghaili@tencent.com>
2024-04-07 12:25:31 -0400

    update

--- Program.cs
+++ Program.cs
@@ -1,16 +1,9 @@
-#define DEFAULT // ALT DEFAULT
-#if NEVER
-#elif DEFAULT
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ContactManager.Data;
 using Microsoft.AspNetCore.Authorization;
 using ContactManager.Authorization;
 
-// snippet3 used in next define
-#region snippet4  
-#region snippet2
-#region snippet
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -22,7 +15,6 @@ builder.Services.AddDefaultIdentity<IdentityUser>(
     options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-#endregion
 
 builder.Services.AddRazorPages();
 
@@ -32,7 +24,6 @@ builder.Services.AddAuthorization(options =>
         .RequireAuthenticatedUser()
         .Build();
 });
-#endregion
 
 // Authorization handlers.
 builder.Services.AddScoped<IAuthorizationHandler,
@@ -59,7 +50,6 @@ using (var scope = app.Services.CreateScope())
 
    await SeedData.Initialize(services, testUserPw);
 }
-#endregion
 
 if (app.Environment.IsDevelopment())
 {
@@ -83,66 +73,3 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
-#elif ALT
-#region snippet3
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using ContactManager.Data;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
-
-var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(
-    options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddRazorPages();
-
-builder.Services.AddControllers(config =>
-{
-    var policy = new AuthorizationPolicyBuilder()
-                     .RequireAuthenticatedUser()
-                     .Build();
-    config.Filters.Add(new AuthorizeFilter(policy));
-});
-
-var app = builder.Build();
-#endregion
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    SeedData.Initialize(services);
-}
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
-#endif
